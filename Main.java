60168ca96913c00b5d08e32ef3852725f228b595
Steven Kang <stevenhanwen.kang@outlook.com>
2024-02-12 22:23:00 -0500

    created methods to sort large integer array using bubblesort

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        testBubbleSortSmallData();
+        //testBubbleSortSmallData();
+
+        testBubbleSortLargeData();
     }
 
     // bubblesort sorts the int array using bubblesort.
@@ -21,6 +27,73 @@ public class Main {
             }
         }
     }    
+
+    // testBubbleSortLargeData sorts the large data using bubblesort.
+    public static void testBubbleSortLargeData() {
+        int[] array1k = getIntegerArrayFromFile("/Users/stevenkang/Documents/code/learn-sorting-algorithms/testdata/1k.dat");
+        long start = System.nanoTime();
+        bubbleSort (array1k);
+        long finish = System.nanoTime();
+        long timeElapsed = finish - start;
+        System.out.println("Sorting 1k data strand runtime: " + timeElapsed/1000000 + " milliseconds");
+        
+        //System.out.println("sorted");
+        //printArray(array1k);
+
+        int[] array5k = getIntegerArrayFromFile("/Users/stevenkang/Documents/code/learn-sorting-algorithms/testdata/5k.dat");
+        start = System.nanoTime();
+        bubbleSort (array5k);
+        finish = System.nanoTime();
+        timeElapsed = finish - start;
+        System.out.println("Sorting 5k data strand runtime: " + timeElapsed/1000000 + " milliseconds");
+
+        //System.out.println("sorted");
+        //printArray(array5k);
+    }
+    
+    // getIntegerArrayFromFile takes a filename and returns an integer array. 
+    static int[] getIntegerArrayFromFile(String filename) {
+        String str = readUsingBufferedReader(filename);
+        String[] splitArray = str.split(",");
+        int[] array = new int[splitArray.length];
+ 
+        // parsing the String argument as a signed decimal
+        // integer object and storing that integer into the
+        // array
+        for (int i = 0; i < splitArray.length; i++) {
+            array[i] = Integer.parseInt(splitArray[i]);
+        }
+        return array;
+    }
+
+    // readUsingBufferedReader takes filename and returns the content as a String. 
+    private static String readUsingBufferedReader(String fileName) {
+		BufferedReader reader = null;
+		StringBuilder stringBuilder = new StringBuilder();
+
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+			String line = null;
+			String ls = System.getProperty("line.separator");
+			while ((line = reader.readLine()) != null) {
+				stringBuilder.append(line);
+				stringBuilder.append(ls);
+			}
+			// delete the last ls
+			stringBuilder.deleteCharAt(stringBuilder.length() - 1);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null)
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		}
+
+		return stringBuilder.toString();
+	}
 
     // testBubbleSortSmallData tests the bubbleSort method using small size data. 
     public static void testBubbleSortSmallData() {
